491a3dec9c047d98476a18c07c98beba48c6a226
KhaldiFatima <>
2023-11-05 13:42:02 +0100

    U

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -14,14 +14,16 @@ const sendEmail = async (
   // Create Email Transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: 587,
+    // port: 587,
+    port: 465,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
-    tls: {
-      rejectUnauthorized: false,
-    },
+    // tls: {
+    //   rejectUnauthorized: false,
+    // },
+    secure: true,
   });
 
   const handlebarOptions = {
